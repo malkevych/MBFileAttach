a0f478132cb8f3202fe29a2b32702f312338e6d3
Malkevych Bohdan Ihorovych <Bohdan@MacBook-Pro-oivrafiv-2.local>
2016-04-18 01:42:57 +0300

    initial project

---
+++ MBFileAttach/MBFileAttach/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MBFileAttach
+//
+//  Created by Malkevych Bohdan Ihorovych on 18.04.16.
+//  Copyright © 2016 MBI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
